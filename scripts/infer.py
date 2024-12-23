1aea9bfc15c7e89b09817e462ccd64148a69a7fd
kohbanye <kohbanye@gmail.com>
2024-12-24 01:50:09 +0900

    move to scripts dir

---
+++ scripts/infer.py
@@ -0,0 +1,33 @@
+import os
+import random
+from pprint import pprint
+
+from rdkit import Chem, RDLogger
+
+from ..generation import Generator
+
+ROOT_DIR = os.environ["ROOT_DIR"]
+RDLogger.DisableLog("rdApp.error")
+
+
+def main():
+    is_chiral = True
+    model_dir = os.path.join(ROOT_DIR, "model", "chiral" if is_chiral else "no_chiral")
+
+    generator = Generator(model_dir)
+
+    smiles_list = generator.batch_generate(10000)
+    pprint(smiles_list[:10])
+    mols = [Chem.MolFromSmiles(s) for s in smiles_list]
+    mols = [m for m in mols if m is not None]
+    print(len(mols) / len(smiles_list))
+
+    filename = "generated_chiral.smi" if is_chiral else "generated_no_chiral.smi"
+    with open(filename, "w") as f:
+        canonical_smiles_list = [Chem.MolToSmiles(m) for m in mols]
+        canonical_smiles_list = random.sample(canonical_smiles_list, 1000)
+        f.write("\n".join(canonical_smiles_list))
+
+
+if __name__ == "__main__":
+    main()
